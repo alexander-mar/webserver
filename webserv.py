9ae1eefea01168eea21359506ec761f82dcd7d97
Alexander Marrapese <amar6637@uni.sydney.edu.au>
2020-11-24 23:07:37 +0800

    x

--- webserv.py
+++ webserv.py
@@ -167,8 +167,8 @@ def cgi(client, file_extension, filepath, execpath):
         os.close(r)
         #client.send(status_200(" ", data_read).encode())
         client.send("HTTP/1.1 200 OK\n".encode())
-        #if not("Content-Type" in data_read):
-         #   client.send("Content-Type: \n".encode())
+        if not("Content-Type" in data_read):
+           client.send("Content-Type: \n".encode())
         client.send("\n".encode())
         client.sendall(data_read.encode())
         client.close()
@@ -183,7 +183,9 @@ def cgi(client, file_extension, filepath, execpath):
         if (os.path.isfile(filepath)):
             try:
                 os.execv(execpath, (execpath, filepath))
-            except OSError:
+            except FileNotFoundError:
+                os.close(w)
+                os.close(r)
                 client.send(status_505(file_extension).encode())
             finally:
                 client.close()
